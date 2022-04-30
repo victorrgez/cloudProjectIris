bf30980b9c7fb77d7308f8504657a12fe7329a06
victorrgez <victorrodriguezcoria@gmail.com>
2022-04-30 13:52:34 +0200

    docker-compose.yml

--- mysql3306/setup.sql
+++ mysql3306/setup.sql
@@ -1,5 +1,5 @@
-CREATE DATABASE irisdatabase;
+CREATE DATABASE IF NOT EXISTS irisdatabase;
 use irisdatabase;
-CREATE TABLE iristable (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sepalLength FLOAT NOT NULL, sepalWidth FLOAT NOT NULL, petalLength FLOAT NOT NULL, petalWidth FLOAT NOT NULL, predictedFlower Varchar (20), confidence FLOAT);
+CREATE TABLE IF NOT EXISTS iristable (id INT NOT NULL AUTO_INCREMENT PRIMARY KEY, sepalLength FLOAT NOT NULL, sepalWidth FLOAT NOT NULL, petalLength FLOAT NOT NULL, petalWidth FLOAT NOT NULL, predictedFlower Varchar (20), confidence FLOAT);
 GRANT ALL PRIVILEGES ON irisdatabase.iristable TO 'webapp'@'%' WITH GRANT OPTION;
 FLUSH PRIVILEGES;
